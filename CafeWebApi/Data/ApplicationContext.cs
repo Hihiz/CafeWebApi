c938d54969a7ee07e59900252e6866342c4fde2e
Hihiz <public@mail.ru>
2023-01-16 21:09:15 +0300

    Контекст бд

---
+++ CafeWebApi/Data/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CafeWebApi.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Breakfast> Breakfasts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Breakfast>().HasData(
+                 new Breakfast[]
+                 {
+                    new Breakfast { Id=1, Name="Суп", Type = "Мясной", Description = "Суп являются основным первым блюдом любого обеденного комплекса. Каждая кухня мира предлагает множество разнообразных рецептов, которые кардинально отличаются. Это позволяет не только насытить организм, но и разнообразить свой рацион, удовлетворив вкусовые предпочтения даже наиболее изысканных гурманов."},
+                    new Breakfast { Id=2, Name="Соус", Type = "Сырный", Description= "Соус (в перевод с французского языка sauce означает «поливку») представляет собой дополнение к блюду/гарниру. С помощью него удается сделать блюдо более привлекательным и повысить его калорийность."},
+                    new Breakfast { Id=3, Name="Борщ", Type = "Зеленый", Description = "Одним из наиболее любимых видов первых блюд большинства людей считается борщ."}
+                 });
+        }
+    }
+}
---
+++ CafeWebApi/Data/Breakfast.cs
@@ -0,0 +1,11 @@
+﻿namespace CafeWebApi.Data
+{
+    public class Breakfast
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+    }
+}
