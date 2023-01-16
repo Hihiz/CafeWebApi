6c7f620e291e18333c9481bcfc16681474b301e5
Hihiz <public@mail.ru>
2023-01-16 21:10:02 +0300

    REST API

---
+++ CafeWebApi/Controllers/BreakfastController.cs
@@ -0,0 +1,77 @@
+﻿using CafeWebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreakfastController : ControllerBase
+    {
+
+        private readonly ApplicationContext _db;
+
+        public BreakfastController(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Breakfast>>> Get()
+        {
+            return Ok(await _db.Breakfasts.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Breakfast>>> Get(int id)
+        {
+            var breakfast = await _db.Breakfasts.FindAsync(id);
+
+            if (breakfast == null)
+                return BadRequest("Breakfast not found");
+
+            return Ok(breakfast);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Breakfast>>> AddBreakfast(Breakfast addBreakfast)
+        {
+            _db.Breakfasts.AddAsync(addBreakfast);
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.Breakfasts.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Breakfast>>> UpdateBreakfast(Breakfast request)
+        {
+            var breakfast = await _db.Breakfasts.FindAsync(request.Id);
+
+            if (breakfast == null)
+                return BadRequest("Breakfast not found");
+
+            breakfast.Name = request.Name;
+            breakfast.Type = request.Type;
+            breakfast.Type = request.Type;
+            breakfast.Description = request.Description;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.Breakfasts.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Breakfast>>> Delete(int id)
+        {
+            var breakfast = await _db.Breakfasts.FindAsync(id);
+
+            if (breakfast == null)
+                return BadRequest("Breakfast not found");
+
+            _db.Breakfasts.Remove(breakfast);
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.Breakfasts.ToListAsync());
+        }
+    }
+}
--- CafeWebApi/Program.cs
+++ CafeWebApi/Program.cs
@@ -1,36 +1,35 @@
+using CafeWebApi.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace CafeWebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+    options.UseSqlite(builder.Configuration.GetConnectionString("ConnectionSqlite"));
+});
 
 
-            app.MapControllers();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            app.Run();
-        }
-    }
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
